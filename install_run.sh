4c4172b6a6fbec21d87ce04cfea9d05d2d08624d
Jacob France <jacob.n.france@gmail.com>
2024-03-15 14:00:56 -0500

    Changed string comparison

--- install_run.sh
+++ install_run.sh
@@ -26,20 +26,20 @@ notif "3) Full install (Includes optional module [devtools, WMs, etc...])"
 notif "4) Custom install"
 
 read USER_INP
-if [[ USER_INP == "1" ]]
+if [[ "$USER_INP" == "1" ]]
 then
     notif "Starting Core install of solarOS using $PACKAGE_MANAGER"
     source "${MODULE_ROOT}preinstall.sh"
     source "${MODULE_ROOT}core.sh"
     source "${MODULE_ROOT}daemons.sh"
-else if [[ USER_INP == "2" ]]
+else if [[ "$USER_INP" == "2" ]]
 then
     notif "Starting Standard install of solarOS using $PACKAGE_MANAGER"
     source "${MODULE_ROOT}preinstall.sh"
     source "${MODULE_ROOT}core.sh"
     source "${MODULE_ROOT}daemons.sh"
     source "${MODULE_ROOT}programs.sh"
-else if [[ USER_INP == "3" ]]
+else if [[ "$USER_INP" == "3" ]]
 then
     notif "Starting Full install of solarOS using $PACKAGE_MANAGER"
     source "${MODULE_ROOT}preinstall.sh"
@@ -47,7 +47,7 @@ then
     source "${MODULE_ROOT}daemons.sh"
     source "${MODULE_ROOT}programs.sh"
     source "${MODULE_ROOT}optional.sh"
-else if [[ USER_INP == "4" ]]
+else if [[ "$USER_INP" == "4" ]]
     notif "Starting Custom install of solarOS using $PACKAGE_MANAGER"
     selectModules
 fi
