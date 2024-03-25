5280ea04d2e63f00a9ddcfdfff8a5ee841b3c090
Jacob France <jacob.n.france@gmail.com>
2024-03-25 14:32:18 -0500

    added xrdp to programs

--- src/modules/programs.sh
+++ src/modules/programs.sh
@@ -8,11 +8,17 @@ eval "$installer xterm alacritty"
 
 # --- accessories ---
 
-eval "$installer nmap vlc thunar htop firefox-esr tldr"
+eval "$installer nmap vlc thunar htop firefox-esr tldr notepadqq"
 
 # --- languages ---
 
 eval "$installer lua python3"
+
+# --- Remote Desktop ---
+
+eval "$installer xrdp"
+
+sudo systemctl stop xrdp
 
 # --- nvim ---
 
